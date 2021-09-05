75b811b4e6062c74d1779ab6d0b3a11fc74e41dc
kimjunsoo <rlawnstn01023@naver.com>
2021-09-05 12:56:58 +0900

    Initial Commit

---
+++ UI컴포넌트 테스트 앱/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UI컴포넌트 테스트 앱
+//
+//  Created by 준수김 on 2021/09/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
